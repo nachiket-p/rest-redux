28fcef19715dc59c55b3bed500b9c79b3bc92e99
nachiket <patel.nachiket.r@gmail.com>
2017-06-22 18:26:46 +0530

    added dummy data in example server

---
+++ example/server/server/boot/dummy-data.js
@@ -0,0 +1,41 @@
+
+
+
+module.exports = function (app) {
+  //d   `1ata sources
+  var ds = app.dataSources['db'];
+
+  const createRoles = (cb) => {
+    cb(null, [])
+  }
+
+  const createUsers = (roles, cb) => {
+    ds.automigrate('User', function (err) {
+      if (err) return cb(err);
+      var User = app.models.User;
+      User.create([
+        { username: 'John', email: 'john@doe.com', password: 'gotthemilk' },
+        { username: 'Jane', email: 'jane@doe.com', password: 'gotthemilk' },
+      ], cb);
+    });
+  }
+  // {"email": "john@doe.com", "password": "gotthemilk" }
+  const createTodos = (users, cb) => {
+    ds.automigrate('Todo', function (err) {
+      if (err) return cb(err);
+      var Todo = app.models.Todo;
+      Todo.create([
+        { text: 'Remember the milk', completed: false },
+        { text: 'Reminder to remember the milk', completed: false },
+        { text: 'Visualize milk as beer', completed: false },
+      ], cb);
+    });
+  }
+
+  createRoles((err, roles) => 
+    createUsers(roles, (err,users) =>
+      createTodos(roles, (err,todos) => console.log('SEED DATA INIT DONE'))
+    )
+  )
+}
+  
