72c270a230fa6524cc51c33fc25bcb10f54765f7
nachiket <patel.nachiket.r@gmail.com>
2017-06-22 19:24:27 +0530

    example fix: update count after deleteAll

--- example/src/todos/actions.js
+++ example/src/todos/actions.js
@@ -15,19 +15,19 @@ export function setVisibilityFilter(filter) {
   return { type: SET_VISIBILITY_FILTER, filter }
 }
 
-
 export const ActionLinks = {
   //DELETE_ALL: () => (dispatch) => dispatch(todoActions.delete({})),
-  DELETE_ALL: () => (dispatch) => dispatch(todoActions.custom('DELETE_ALL','deleteTodos', 'POST')).then(response=> {
-      dispatch(todoActions.find({}))
+  DELETE_ALL: () => (dispatch) => dispatch(todoActions.custom('DELETE_ALL', 'deleteTodos', 'POST')).then(response => {
+    dispatch(todoActions.find({}))
+    dispatch(todoActions.count({}))
   }),
   COMPLETE_ALL: () => (dispatch) => {
-    dispatch(todoActions.updateAll({completed:false}, {completed: true})).then(response=> {
+    dispatch(todoActions.updateAll({ completed: false }, { completed: true })).then(response => {
       dispatch(todoActions.find({}))
     })
   },
   UNCOMPLETE_ALL: () => (dispatch) => {
-    dispatch(todoActions.updateAll({completed:true}, {completed: false})).then(response => {
+    dispatch(todoActions.updateAll({ completed: true }, { completed: false })).then(response => {
       dispatch(todoActions.find({}))
     })
   }
