0609204d91fce629fd27c441c4516dd6a7439b4d
Raghav Agarwal <agarwalraghav01@outlook.com>
2017-07-24 17:41:52 +0530

    Added test for hasPrev and hasNext in list selector

--- tests/unit/list/selectors/list.test.js
+++ tests/unit/list/selectors/list.test.js
@@ -2,15 +2,59 @@ import listSelectors from '../../../../src/list/listSelectors'
 import modelSelectors from '../../../../src/model/modelSelectors'
 import { DEFAULT_CONFIG } from '../../../../src'
 import _ from 'lodash'
+const data = {
+	'1': {
+					text: 'Remember the milk',
+					completed: false,
+					id: 1
+				},
+				'2': {
+					text: 'Reminder to remember the milk',
+					completed: false,
+					id: 2
+				},
+				'3': {
+					text: 'Visualize milk as beer',
+					completed: false,
+					id: 3
+				},
+				'4': {
+					text: 'Don\'t forget the milk at the store',
+					completed: false,
+					id: 4
+				},
+				'5': {
+					text: 'fifth',
+					completed: false,
+					id: 5
+				},
+				'6': {
+					text: 'sixth',
+					completed: false,
+					id: 6
+				},
+				'7': {
+					text: 'seventh',
+					completed: false,
+					id: 7
+				}
+}
 const STATE_FIRST_PAGE = {
 	rest: {
 		todos: {
 			instances: {
-				'1': { name: 'todos' },
-				'2': { name: 'users' }
+				data
 			},
 			last: {
-				find: [],
+				find: [
+					1,
+					2,
+					3,
+					4,
+					5,
+					6,
+					7
+				],
 				'delete': [],
 				custom: {}
 			},
@@ -23,7 +67,7 @@ const STATE_FIRST_PAGE = {
 					offset: 0,
 					pageSize: 3,
 					total: 7,
-					result: [1, 2],
+					result: [1, 2, 3],
 					headers: {},
 					params: {
 						completed: true
@@ -34,15 +78,22 @@ const STATE_FIRST_PAGE = {
 	}
 }
 
-const STATE_SECOND_PAGE =  {
+const STATE_MIDDLE_PAGE = {
 	rest: {
 		todos: {
 			instances: {
-				'1': { name: 'todos' },
-				'2': { name: 'users' }
+			data
 			},
 			last: {
-				find: [],
+				find: [
+					1,
+					2,
+					3,
+					4,
+					5,
+					6,
+					7
+				],
 				'delete': [],
 				custom: {}
 			},
@@ -52,10 +103,10 @@ const STATE_SECOND_PAGE =  {
 			error: null,
 			lists: {
 				completed: {
-					offset: 1,
+					offset: 3,
 					pageSize: 3,
 					total: 7,
-					result: [1, 2],
+					result: [4, 5, 6],
 					headers: {},
 					params: {
 						completed: true
@@ -66,15 +117,22 @@ const STATE_SECOND_PAGE =  {
 	}
 }
 
-const STATE_THIRD_PAGE =  {
+const STATE_LAST_PAGE = {
 	rest: {
 		todos: {
 			instances: {
-				'1': { name: 'todos' },
-				'2': { name: 'users' }
+			data
 			},
 			last: {
-				find: [],
+				find: [
+					1,
+					2,
+					3,
+					4,
+					5,
+					6,
+					7
+				],
 				'delete': [],
 				custom: {}
 			},
@@ -84,10 +142,10 @@ const STATE_THIRD_PAGE =  {
 			error: null,
 			lists: {
 				completed: {
-					offset: 2,
+					offset: 6,
 					pageSize: 3,
 					total: 7,
-					result: [1, 2],
+					result: [7],
 					headers: {},
 					params: {
 						completed: true
@@ -97,6 +155,7 @@ const STATE_THIRD_PAGE =  {
 		}
 	}
 }
+
 
 
 const modelSelector = modelSelectors({ modelName: 'todos' }, {}, DEFAULT_CONFIG.rootSelector)
@@ -113,7 +172,7 @@ describe('List Selector', () => {
 	})
 	it('should return instaces', () => {
 		const instances = STATE_FIRST_PAGE.rest.todos.instances
-		expect(selectors.getInstances(STATE_FIRST_PAGE)).toEqual([instances['1'], instances['2']])
+		expect(selectors.getInstances(STATE_FIRST_PAGE)).toEqual([instances['1'], instances['2'], instances['3']])
 	})
 	it('should return get curent page', () => {
 		const listObj = selectors.getListObj(STATE_FIRST_PAGE)
@@ -125,15 +184,14 @@ describe('List Selector', () => {
 		expect(selectors.getPages(STATE_FIRST_PAGE)).toEqual([...Array(pages)].map((_, i) => i++))
 	})
 	it('should return has Next', () => {
-		const listObj = selectors.getListObj(STATE_FIRST_PAGE)
 		expect(selectors.hasNext(STATE_FIRST_PAGE)).toEqual(true)
-		expect(selectors.hasNext(STATE_SECOND_PAGE)).toEqual(true)
-		expect(selectors.hasNext(STATE_THIRD_PAGE)).toEqual(true)
+		expect(selectors.hasNext(STATE_MIDDLE_PAGE)).toEqual(true)
+		expect(selectors.hasNext(STATE_LAST_PAGE)).toEqual(false)
 	})
 	it('should return has prev', () => {
 		expect(selectors.hasPrev(STATE_FIRST_PAGE)).toEqual(false)
-		expect(selectors.hasPrev(STATE_SECOND_PAGE)).toEqual(true)
-		expect(selectors.hasPrev(STATE_THIRD_PAGE)).toEqual(true)
+		expect(selectors.hasPrev(STATE_MIDDLE_PAGE)).toEqual(true)
+		expect(selectors.hasPrev(STATE_LAST_PAGE)).toEqual(true)
 	})
 })
 
